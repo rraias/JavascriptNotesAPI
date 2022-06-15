3868fba9c0ab4c252403673d143c02eec5d061ce
Raphael <raphaelraia@outlook.com.br>
2022-06-14 23:43:20 -0300

    changed to localhost

--- config/database.js
+++ config/database.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-require('dotenv').config();
-const MONGO_URL = process.env.MONGO_URL;
-
-mongoose.connect(MONGO_URL, {
+mongoose.connect('mongodb://localhost/javascriptNote', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('Connection succesful'))
